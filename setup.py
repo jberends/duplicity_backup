779cc1bbea007f5dceb3b9f57c98c1d49725558a
Jochem Berends <jochem.berends@ke-works.com>
2019-06-03 15:29:48 +0200

    also add envparse to setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md") as readme_file:
 with open("CHANGELOG.md") as history_file:
     history = history_file.read()
 
-requirements = ["Click>=6.0", "PyYAML"]
+requirements = ["Click>=6.0", "PyYAML", "envparse"]
 
 setup_requirements = []
 
