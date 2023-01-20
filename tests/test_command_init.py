7bfcf046a005aa6d9dd22b0d40e10363d07d6abb
Jochem Berends <jochem.berends@ke-works.com>
2023-01-20 15:50:20 +0100

    updated testing

--- tests/test_command_init.py
+++ tests/test_command_init.py
@@ -1,10 +1,15 @@
+from click import Path
+
 from duplicity_backup_s3.cli import duplicity_backup_s3
+from duplicity_backup_s3.utils import temp_chdir
 from tests.test_base import TestSetup
 
 
 class TestCommandInit(TestSetup):
     def test_init_create_empty_config(self):
-        with self.runner.isolated_filesystem():
+        with temp_chdir():
             # no config file there!
-            result = self.runner.invoke(duplicity_backup_s3, ["init"])
+            result = self.runner.invoke(
+                duplicity_backup_s3, ["init", "--quiet", "--verbose"]
+            )
             self.assertEqual(result.exit_code, 0)
